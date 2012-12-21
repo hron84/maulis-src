812e4108a245a23aa50b96bfc84d6e7c20636f5c
Garami Gábor <hron@hron.me>
2012-12-21 18:59:17 +0100

    Initial commit

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+DATE="`date +"%F %T"`"
+export GIT_AUTHOR_NAME="Adam Maulis"
+export GIT_AUTHOR_EMAIL="maulis@ludens.elte.hu"
+
+git checkout ftp
+wget -q -r -np http://maulis.web.elte.hu/src/
+find . -type f -iname 'index.html*' -delete
+cp -r maulis.web.elte.hu/src/* .
+rm -fr maulis.web.elte.hu
+git add . 
+git commit -m "Autoupdate ${DATE}"
